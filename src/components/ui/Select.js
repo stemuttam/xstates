77df077ebf82ed9379742d9bccfa2e99698e3e0f
Uttam Pal <stem.uttam@gmail.com>
2025-02-21 00:15:53 +0530

    Delete src directory

--- src/components/ui/Select.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Select = ({ children, ...props }) => {
-  return <select className="border p-2 rounded w-full" {...props}>{children}</select>;
-};
-
-export default Select;
